0ac9fccb0f350580374b3d13bb6bc4e184997b46
cometbid-project <20684020+cometbid-project@users.noreply.github.com>
2023-05-03 01:49:47 -0400

    first commit

---
+++ src/main/java/com/polarbookshop/configservice/ConfigServiceApplication.java
@@ -0,0 +1,20 @@
+package com.polarbookshop.configservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+/**
+ * 
+ * @author Gbenga
+ *
+ */
+@SpringBootApplication
+@EnableConfigServer
+public class ConfigServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConfigServiceApplication.class, args);
+	}
+
+}
